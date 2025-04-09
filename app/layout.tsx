ab0645c4521bed9ca03bcc1bd239d3891c2f2b3a
storm <khameleonstorm@gmail.com>
2025-04-09 15:05:08 +0100

    Seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,47 +4,44 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
 export const metadata: Metadata = {
-  title: "JENNAFER BEAVER TURNER",
-  description:
-    "Welcome to Financial Coach Hub, led by JENNAFER BEAVER TURNER. We're dedicated to helping you achieve financial success through personalized coaching and expert guidance. Take control of your finances, build wealth, and reach your goals with us by your side. Start your journey to financial freedom today!",
+	title: "JENNAFER BEAVER TURNER",
+	description:
+		"Welcome to Financial Coach Hub, led by JENNAFER BEAVER TURNER. We're dedicated to helping you achieve financial success through personalized coaching and expert guidance. Take control of your finances, build wealth, and reach your goals with us by your side. Start your journey to financial freedom today!",
 };
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter",
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
 });
 
 const panchang = localFont({
-  src: "../public/fonts/Panchang-Variable.ttf",
-  display: "swap",
-  variable: "--font-panchang",
+	src: "../public/fonts/Panchang-Variable.ttf",
+	display: "swap",
+	variable: "--font-panchang",
 });
 
 const montserrat = localFont({
-  src: "../public/fonts/Montserrat-VariableFont_wght.ttf",
-  display: "swap",
-  variable: "--font-montserrat",
+	src: "../public/fonts/Montserrat-VariableFont_wght.ttf",
+	display: "swap",
+	variable: "--font-montserrat",
 });
 
 const bagero = localFont({
-  src: "../public/fonts/Bagero.otf",
-  display: "swap",
-  variable: "--font-bagero",
+	src: "../public/fonts/Bagero.otf",
+	display: "swap",
+	variable: "--font-bagero",
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${panchang.variable} ${montserrat.variable} ${bagero.variable}`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<meta name="google-site-verification" content="ResSvjtHzn9ajHjALu7JesJc8foMMhzBUgy2PunTOwk" />
+			</head>
+			<body className={`${inter.variable} ${panchang.variable} ${montserrat.variable} ${bagero.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
 }
